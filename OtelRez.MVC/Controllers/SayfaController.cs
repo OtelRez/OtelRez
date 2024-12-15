f4080109c2878135ef7e2e5cce4fdb91601bc56c
betulranaozer <brana255@gmail.com>
2024-12-15 22:53:00 +0300

    İletişime geç alanının controllerı yazıldı

--- OtelRez.MVC/Controllers/SayfaController.cs
+++ OtelRez.MVC/Controllers/SayfaController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OtelRez.BL.Managers.Abstract;
 using OtelRez.Entity.Entities.Concrete;
+using OtelRez.MVC.Models.VMs.Hesap;
 using OtelRez.MVC.Models.VMs.Sayfa;
 
 namespace OtelRez.MVC.Controllers
@@ -18,15 +19,31 @@ namespace OtelRez.MVC.Controllers
         [AllowAnonymous]
         public IActionResult Iletisim()
         {
-
-            return View();
+            IletisimeGecVM iletisimeGecVM = new IletisimeGecVM();
+            return View(iletisimeGecVM);
         }
 
         [HttpPost]
         [AllowAnonymous]
         public async Task<IActionResult> Iletisim(IletisimeGecVM iletisimeGecVM)
         {
-            return
+            if (!ModelState.IsValid)
+            {
+                notyfService.Error("Düzeltilmesi gereken yerler var");
+                return View(iletisimeGecVM);
+            }
+
+            IletisimeGec iletisimeGec = new IletisimeGec();
+            iletisimeGec.Adi = iletisimeGecVM.Adi;
+            iletisimeGec.Mail= iletisimeGecVM.Mail;
+            iletisimeGec.Konu = iletisimeGecVM.Konu;
+            iletisimeGec.Mesaj = iletisimeGecVM.Mesaj;
+
+            iletisimeGecManager.Create(iletisimeGec);
+
+            notyfService.Success("İşlem Başarılı");
+
+            return RedirectToAction("Iletisim", "Sayfa");
         }
 
         public IActionResult Odalar()
