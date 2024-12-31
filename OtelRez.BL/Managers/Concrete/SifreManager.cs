77b4213e1bf290460cc956ebc5d11f65368cf81e
EmreAndac <emreandac5252@hotmail.com>
2024-12-31 15:09:38 +0300

    Şifremi unuttum sayfası yapımı başladı

---
+++ OtelRez.BL/Managers/Concrete/SifreManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtelRez.BL.Managers.Concrete
+{
+    public class SifreManager
+    {
+        public static void SendEmail(string toEmail, string subject, string body)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("sizin-email@gmail.com", "sizin-email-şifreniz"),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("sizin-email@gmail.com"),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(toEmail);
+
+            try
+            {
+                smtpClient.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                // Hata yönetimi
+                throw new Exception("E-posta gönderilirken bir hata oluştu.", ex);
+            }
+        }
+    }
+}
